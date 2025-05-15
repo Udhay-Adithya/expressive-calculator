bcd5d2ade14e3630276395033bb79c828b174047
Udhay-Adithya <udhayxd@gmail.com>
2025-05-15 22:38:16 +0530

    feat: add basic calculator states
    This commit introduces the core data models for the calculator:
    
    - `CalculatorState`: Represents the current state of the calculator, including the expression and result.
    - `Operations`: Defines an enum for all supported calculator operations.
    
    Additionally, it updates the following library versions:
    - Android Gradle Plugin to 8.8.2
    - Lifecycle Runtime KTX to 2.9.0
    - Compose BOM to 2025.05.00

---
+++ app/src/main/java/com/udhay/skeucalc/model/CalculatorState.kt
@@ -0,0 +1,6 @@
+package com.udhay.skeucalc.model
+
+data class CalculatorState(
+    val expression:String = "",
+    val result:String = ""
+)
---
+++ app/src/main/java/com/udhay/skeucalc/model/Operations.kt
@@ -0,0 +1,11 @@
+package com.udhay.skeucalc.model
+
+enum class Operations(val symbol:String) {
+    Add("+"),
+    Subtract("-"),
+    Multiply("×"),
+    Divide("÷"),
+    Clear("C"),
+    Equals("=")
+}
+
